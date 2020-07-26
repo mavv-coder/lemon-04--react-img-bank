0c7a8b968fe205c87575e84dc48d6d6f7985d36d
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-07-26 20:13:55 +0200

    hidding cart working

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./app";
 import { AppContextProvider } from "./core/context";
+import { Navbar } from "./layout/components";
 
 ReactDOM.render(
   <div>
     <AppContextProvider>
+      <Navbar />
       <App />
     </AppContextProvider>
   </div>,
---
+++ src/layout/components/navbar.component.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { useAppContext } from "../../core/context";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    display: "flex",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  icon: {
+    fontSize: "30px",
+  },
+}));
+
+export const Navbar: React.FC = () => {
+  const classes = useStyles();
+  const { setVisibleCart, visibleCart } = useAppContext();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Movie Posters & Album Covers Collection
+          </Typography>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={() => setVisibleCart(!visibleCart)}
+          >
+            <ShoppingCartOutlinedIcon className={classes.icon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
